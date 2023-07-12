2f3e19880cf98f1b3e27d00c01ba523bfba980bf
1378463428@qq.com <caonimade000000>
2023-07-12 13:34:09 +0800

    rouge-n指标计算

--- theshy/metrics/rouge_.py
+++ theshy/metrics/rouge_.py
@@ -7,33 +7,61 @@
 # @System  : Windows
 # @desc    : rouge指标计算
 
-# rouge包，pip install rouge安装
-from rouge import Rouge
 
-def sentence_level_rouge_n(list_reference, candidate):
-    scores = {"rouge-1": {}}
+
+def sentence_level_rouge_n(list_reference, candidate, n=None):
+    # 默认计算rouge 1-4
+    if n is None:
+        n = [1, 2, 3, 4]
+
+    scores = {}
+    for i in n:
+        refer_count, cand_count, overlap_count = number_statistics(i, list_reference, candidate)
+        scores.update(rouge_n(i, refer_count, cand_count, overlap_count))
+
+    return scores
+
+
+def rouge_n(n, refer_count, cand_count, overlap_count):
+    p, r = overlap_count / cand_count, overlap_count / refer_count
+    return {
+        f'rouge-{n}': {
+            'precision': p,
+            'recall': r,
+            'f1': 2 * p * r / (p + r + 1e-8) if p + r != 0 else 0,
+        }
+    }
+
+
+def number_statistics(n, reference, candidate):
+    refer = []
+    for i in range(n, len(reference)+1):
+        refer.append(' '.join(reference[i-n: i]))
     # 对于重复文本只看做一次，所以直接取set
-    candidate_set = set(candidate)
-    list_reference_set = set(list_reference)
+    refer = set(refer)
 
-    overlap = sum([1 if i in list_reference_set else 0 for i in candidate_set])
-    p, r = overlap / len(candidate), overlap / len(list_reference_set)
-    scores['rouge-1']['precision'] = p
-    scores['rouge-1']['recall'] = r
-    scores['rouge-1']['f1'] = 2 * p * r / (p + r) if p + r != 0 else 0
+    cand = []
+    for i in range(n, len(candidate)+1):
+        cand.append(' '.join(candidate[i-n: i]))
+    cand = set(cand)
+
+    overlap = sum([1 if i in refer else 0 for i in cand])
+
+    return len(refer), len(cand), overlap
 
 
-    return
+if __name__ == '__main__':
+    references1 = ['my', 'first', 'correct', 'sentence', 'sentence', 'penguin']
+    candidates1 = ['my', 'first', 'correct', 'sentence', 'sentence', 'apple', 'tiger', 'penguin']
 
+    candidates = "my first correct sentence sentence penguin"
+    references = "my first correct sentence sentence apple tiger penguin"
 
-references1 = ['my', 'first', 'correct', 'sentence', 'sentence', 'penguin']
-candidates1 = ['my', 'first', 'correct', 'sentence', 'sentence', 'apple', 'tiger', 'penguin']
+    # rouge包，pip install rouge安装
+    from rouge import Rouge
+    rouge = Rouge(metrics=["rouge-1", "rouge-2", "rouge-3", "rouge-4", "rouge-l"])
+    scores = rouge.get_scores(candidates, references)
+    print(scores)
 
-candidates = "my first correct sentence penguin"
-references = "my first correct sentence apple tiger penguin"
-
-rouge = Rouge()
-scores = rouge.get_scores(candidates, references)
-print(scores)
-scores = sentence_level_rouge_n(references1, candidates1)
-print(scores)
+    scores = sentence_level_rouge_n(references1, candidates1)
+    print(scores)
