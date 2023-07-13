2ade7631119d102011f47cb8687c829505e36dd6
1378463428@qq.com <caonimade000000>
2023-07-13 15:54:57 +0800

    rouge-l更新

--- theshy/metrics/rouge_.py
+++ theshy/metrics/rouge_.py
@@ -52,6 +52,28 @@ def number_statistics(n, reference, candidate):
 
 
 def rouge_l(reference, candidate):
+    def rouge_l_text(dp, candidate, text):
+        m = min(len(dp), len(dp[0]))
+        for i in range(m - 1):
+            if dp[i+1][i+1] == dp[i][i] + 1 and dp[i+1][i] == dp[i][i] and dp[i][i+1] == dp[i][i]:
+                text += [candidate[i]]
+            else:
+                begin = i
+                break
+        else:
+            return text
+
+        for j in range(begin+1, len(dp[0]) - 1):
+            if dp[begin+1][j+1] == dp[begin][j] + 1 and dp[begin][j+1] == dp[begin][j] and dp[begin+1][j] == dp[begin][j]:
+                t = rouge_l_text(dp[begin:, j:], candidate[begin:], text.copy())
+                if len(t) == dp[-1][-1]: texts.append(t)
+
+        for i in range(begin+1, len(dp) - 1):
+            if dp[i+1][begin+1] == dp[i][begin] + 1 and dp[i][begin+1] == dp[i][begin] and dp[i+1][begin] == dp[i][begin]:
+                t = rouge_l_text(dp[i:, begin:], candidate[i:], text.copy())
+                if len(t) == dp[-1][-1]: texts.append(t)
+        return text
+
     dp = [[0 for _ in range(len(reference) + 1)] for _ in range(len(candidate) + 1)]
     for i in range(1, len(candidate) + 1):
         for j in range(1, len(reference) + 1):
@@ -59,9 +81,14 @@ def rouge_l(reference, candidate):
                 dp[i][j] = max(dp[i - 1][j - 1] + 1, dp[i - 1][j], dp[i][j - 1])
             else:
                 dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1])
-    rouge_l_text(np.array(dp), [], candidate)
-    p = dp[-1][-1] / len(set(candidate))
-    r = dp[-1][-1] / len(set(reference))
+    texts = []
+    rouge_l_text(np.array(dp), candidate, [])
+    common_len = 0
+    for t in texts:
+        common_len = max(common_len, len(set(t)))
+
+    p = common_len / len(set(candidate))
+    r = common_len / len(set(reference))
     return {
         f'rouge-l': {
             'precision': p,
@@ -71,41 +98,25 @@ def rouge_l(reference, candidate):
     }
 
 
-def rouge_l_text(dp, text, candidate):
-    m = min(len(dp), len(dp[0]))
-    for i in range(m - 1):
-        if dp[i+1][i+1] == dp[i][i] + 1:
-            text.append(candidate[i])
-        else:
-            begin = i
-            break
-    else:
-        return
-
-    for j in range(begin+1, len(dp[0]) - 1):
-        if dp[begin+1][j+1] == dp[begin][j] + 1:
-            rouge_l_text(dp[begin:, j:], text, candidate[j-1:])
-
-    for i in range(begin+1, len(dp) - 1):
-        if dp[i+1][begin+1] == dp[i][begin] + 1:
-            rouge_l_text(dp[i:][begin:], text, candidate[begin:])
-
 
 
 
 
 if __name__ == '__main__':
-    references1 = ['my', 'first', 'correct', 'sentence', 'apple', 'tiger', 'sentence', 'penguin', 'a']
-    candidates1 = ['my', 'first', 'correct', 'sentence', 'sentence', 'apple', 'tiger', 'penguin']
+    references1 = ['my', 'first', 'correct', 'sentence', 'sentence', 'apple', 'tiger', 'penguin']
+    candidates1 = ['my', 'first', 'correct', 'sentence', 'penguin']
 
-    candidates = "my first correct sentence sentence penguin"
+    # references1 = ['a', 'b', 'c', 'd', 'e', 'f', 'd', 'g', 'h']
+    # candidates1 = ['a', 'b', 'c', 'd', 'd', 'e', 'f', 'g']
+
     references = "my first correct sentence sentence apple tiger penguin"
+    candidates = "my first correct sentence sentence penguin"
 
     # rouge包，pip install rouge安装
     from rouge import Rouge
-    rouge = Rouge(metrics=["rouge-1", "rouge-2", "rouge-3", "rouge-4", "rouge-l"])
+    rouge = Rouge(metrics=["rouge-1", "rouge-2", "rouge-l"])
     scores = rouge.get_scores(candidates, references)
     print(scores)
 
-    scores = sentence_level_rouge_n(references1, candidates1)
+    scores = sentence_level_rouge_n(references1, candidates1, n=[1, 2])
     print(scores)
