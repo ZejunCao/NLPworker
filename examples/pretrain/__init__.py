363873e6eeab0ce4e01b473aaa340a75bdf1d1be
ZejunCao <1378463428@qq.com>
2023-12-20 01:35:47 +0800

    预训练模型文件夹

---
+++ examples/pretrain/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author      : Cao Zejun
+# @Time        : 2023/12/20 1:35
+# @File        : __init__.py.py
+# @Software    : Pycharm
+# @description :
